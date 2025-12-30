c6b8577a504edba84c4b26ada52c9cceed0b32aa
Rylan Deacon-Rogers <rylan.deaconrogers@hotmail.com>
2025-12-29 20:14:49 -0800

    Mapty update 1

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,7 +13,26 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation.getCurrentPosition(
   function (position) {
-    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude);
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent);
+    });
   },
   function () {
     alert('Could not get your position');
