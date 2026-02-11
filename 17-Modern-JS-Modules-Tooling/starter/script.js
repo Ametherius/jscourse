589ad104860205e433e02491bd6f30208dd0bb7d
Ametherius <139707007+Ametherius@users.noreply.github.com>
2026-02-11 05:10:08 -0800

    Update

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,18 +1,81 @@
-// import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
-// console.log('Importing module');
+// // import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+// // console.log('Importing module');
 
-import * as ShoppingCart from './shoppingCart.js';
+// import * as ShoppingCart from './shoppingCart.js';
 
-// addToCart('bread', 4);
+// // addToCart('bread', 4);
 
-// console.log(totalPrice, totalQuantity);
+// // console.log(totalPrice, totalQuantity);
 
-console.log(ShoppingCart.totalPrice);
+// console.log(ShoppingCart.totalPrice);
 
-import add, { cart } from './shoppingCart.js';
+// import add, { cart } from './shoppingCart.js';
 
-add('pizzas', 3);
-add('loaf of bread', 1);
-add('jug of milk', 1);
+// add('pizzas', 3);
+// add('loaf of bread', 1);
+// add('jug of milk', 1);
 
-console.log(cart);
+// console.log(cart);
+
+// Imports are not copies of the export, they are live connection
+
+/*
+/////////////////////////////////////////
+// LECTURE 2: TOP LEVEL AWAIT
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+// not clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
+
+/*
+////////////////////////////////
+// MODULE PATTERNS
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizzas', 2);
+
+console.log(ShoppingCart2);
+*/
+
+////////////////////////////////
+// INTRO TO NPM
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -3,12 +3,17 @@ console.log('Exporting module');
 const shippingCost = 10;
 export const cart = [];
 
-export default function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart`);
-}
-
 const totalPrice = 237;
 const totalQuantity = 10;
 
 export { totalPrice, totalQuantity };
+
+const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
